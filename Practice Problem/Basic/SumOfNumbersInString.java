ccf1eb803eb3d32f28976386fb22d42fc6bccda8
saravana-ganesh <saravanaganesh@yandex.com>
2020-11-23 14:58:59 +0530

    SumOfNumbersInString

--- Practice Problem/Basic/SumOfNumbersInString.java
+++ Practice Problem/Basic/SumOfNumbersInString.java
@@ -1,5 +1,40 @@
 package com.codewar;
 import java.util.Scanner;
+/*
+ Given a string str containing alphanumeric characters, calculate sum of all 
+ numbers present in the string.
+
+Input:
+	The first line of input contains an integer T denoting the number of test cases.
+ 	Then T test cases follow. Each test case contains a string containing alphanumeric characters.
+
+Output:
+	Print the sum of all numbers present in the string.
+
+Constraints:
+	1<=T<=105
+	1<=length of the string<=105
+
+Example:
+Input:
+	4
+	1abc23
+	geeks4geeks
+	1abc2x30yz67
+	123abc
+
+Output:
+	24
+	4
+	100
+	123
+
+Explanation:
+	Testcase 1: 
+		1 and 23 are numbers in the string which is added to get the sum as 24.
+	Testcase 4: 123 is a single number, so sum is 123.
+ * 
+ */
 public class SumOfNumbersInString {
 	private static int number=0;
 	public static void main(String[] args) {
